f7382d1cde9b7fe55b3e198884a987d9e5cc5f1b
Lilwero20 <guerovasir13@gmail.com>
2026-07-05 11:07:39 -0600

    Update URL for Speed Keyboard Escape game

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [9584852943] = "https://raw.githubusercontent.com/Lilwero20/Script-Games/refs/heads/main/%252B1%2520Speed%2520Keyboard%2520Escape.lua", -- +1 Speed Keyboard Escape
+  [9584852943] = "https://raw.githubusercontent.com/Lilwero20/Script-Games/refs/heads/main/1SpeedKeyboard", -- +1 Speed Keyboard Escape
 }
 
 return Games
