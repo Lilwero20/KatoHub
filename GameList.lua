511fc4f23ba599bcfdb128a38b622a0638a7de06
Lilwero20 <guerovasir13@gmail.com>
2026-07-21 09:32:54 -0600

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [9584852943] = "https://raw.githubusercontent.com/Lilwero20/Script-Games/refs/heads/main/1SpeedKeyboard", -- +1 Speed Keyboard Escape
+  [9584852943] = "https://raw.githubusercontent.com/Lilwero20/Script-Games/refs/heads/main/+1SpeedKeyboardEscape", -- +1 Speed Keyboard Escape
 }
 
 return Games
