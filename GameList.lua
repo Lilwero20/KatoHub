8986e95ba60f73b76795f6476329e214bce57899
Lilwero20 <guerovasir13@gmail.com>
2026-07-23 14:48:06 -0600

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,6 @@
 local Games = {
   [9584852943] = "https://raw.githubusercontent.com/Lilwero20/Script-Games/refs/heads/main/1SpeedKeyboardEscape", -- +1 Speed Keyboard Escape
+  [78515283254292] = "https://raw.githubusercontent.com/Lilwero20/Script-Games/refs/heads/main/AnimalAnomaly", -- Animal Anomaly
 }
 
 return Games
