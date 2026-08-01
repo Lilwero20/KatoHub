15cfbefdb15bb170fd49b832397f40fc712606d6
Lilwero20 <guerovasir13@gmail.com>
2026-08-01 16:43:23 -0600

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -3,6 +3,7 @@ local Games = {
   [10148749921] = "https://raw.githubusercontent.com/Lilwero20/Script-Games/refs/heads/main/AnimalAnomaly", -- Animal Anomaly
   [142823291] = "https://www.keyforge.win/v1/load/cmsair2yx3saqwdkvhlvboy2t?e=1&script=cmsaldvbd00v5fqkvttrukehm&lsig=ko2ISKb0tQxvggPMd7Dkeyap20acqXsmgpqzuXkLthk", -- MM2
   [4924922222] = "https://www.keyforge.win/v1/load/cmsair2yx3saqwdkvhlvboy2t?e=1&script=cmsaiz0l93tefwdkvp52oolyk&lsig=0EgVsUGV6eNO7YaWbbcM5ullyyZaTGEwG92I2xiRPuU", -- Brookhaven
+  [111958650] = "https://www.keyforge.win/v1/load/cmsair2yx3saqwdkvhlvboy2t?e=1&script=cmsay0gnj2gkw1dkvtok0ageu&lsig=DfMjopHJpOTj-DjwO5Utw8snt4gv5SVi_bhREOFIRYY", -- Arsenal
 }
 
 return Games
